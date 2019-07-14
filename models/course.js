554ae40f90dfd6083fc7695956b11f7e82445614
ziggy-n <49159585+ziggy-n@users.noreply.github.com>
2019-07-15 00:00:36 +0200

    Add files via upload

---
+++ models/course.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = (sequelize, DataTypes) => {
+    const Course = sequelize.define('Course', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        userId: {
+            type: DataTypes.INTEGER,
+            allowNull: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        estimatedTime: {
+            type: DataTypes.STRING,
+            allowNull: true
+        },
+        materialsNeeded: {
+            type: DataTypes.STRING,
+            allowNull: true
+        }
+    });
+
+    Course.associate = (models) => {
+        Course.belongsTo(models.User, {
+            foreignKey: {
+                fieldName: 'userId',
+                allowNull: false
+            }
+        });
+    };
+
+    return Course;
+}
